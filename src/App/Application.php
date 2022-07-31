30967f6ccf62541653ba63b35ddf46be2da4119f
Marco Preciado <mac46005@gmail.com>
2022-07-31 18:11:03 -0500

    update repo

--- src/App/Application.php
+++ src/App/Application.php
@@ -18,13 +18,13 @@ class Application{
         //return static::$db;
     }
 
-    public static run(){
+    public function run(){
         try{
             echo $this->router->resolve($this->request['uri'],strtolower($this->request['method']));
         }catch(RouteNotFoundException){
             http_response_code(404);
 
-            echo View::make('error/404');
+            echo View::create_View('error/404');
         }
     }
 }
