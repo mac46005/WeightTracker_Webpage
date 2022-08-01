6147d5d9b34672767c88a7e3839e417ad3c51e41
Marco Preciado <mac46005@gmail.com>
2022-07-31 21:06:55 -0500

    redo

--- src/exceptions/RouteNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-declare(strict_types = 1);
-
-namespace WghtTrackApp_ClassLib\Exceptions;
-
-class RouteNotFoundException{
-    protected $message = 'Route Not found!';
-}
--- src/exceptions/ViewNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types = 1);
-
-namespace WghtTrackApp_ClassLib\Exceptions;
-
-class ViewNotFoundException{
-    protected string $message = "ERROR: View not found!";
-}
